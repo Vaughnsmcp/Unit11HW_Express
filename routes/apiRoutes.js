b02ff80523ff45a8f6fb210d51eb356be4ccb815
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-02-12 16:49:38 -0800

    got page to save

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,11 +8,22 @@
 
 const noteData = require('../db.json');
 
+const fs = require('fs');
+const util = require('util')
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = app => {
   app.get('/api/notes', (req, res) => res.json(noteData));
+  app.post('/api/notes', (req, res) => {
+    noteData.push(req.body);
+    res.json(true)
+   
+  });
+
+
 };
