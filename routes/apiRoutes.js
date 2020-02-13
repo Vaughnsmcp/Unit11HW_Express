af54378db0e91bee0d89987bdbd783bf8eee0589
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-02-12 17:05:55 -0800

    rewrote db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,10 +9,7 @@
 const noteData = require('../db.json');
 
 const fs = require('fs');
-const util = require('util')
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -21,7 +18,11 @@ module.exports = app => {
   app.get('/api/notes', (req, res) => res.json(noteData));
   app.post('/api/notes', (req, res) => {
     noteData.push(req.body);
-    res.json(true)
+    fs.writeFile('./db.json',JSON.stringify(noteData),err =>{
+      if (err) throw err;
+      console.log("updated")
+    })
+    res.json(true);
    
   });
 
