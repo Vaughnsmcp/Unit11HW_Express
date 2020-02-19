807624fe2bd2089e64f7919875bcd87aedee7397
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-02-18 16:27:20 -0800

    added image to readme

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 #### By _**Vaughn McPherson**_
 
+![TitlePageImage](./public/assets/image/TitlePage.png)
+
 ## Description
-_This application will use an express backend and save and retrieve note data from a JSON file._
+_This application will use an express backend and save and retrieve note data from a JSON file. You will arrive at the browser page. Enter a title for your note, enter note, ave note. Your note title will show up in the left column. Repeat. Option to delete notes by clicking on trash can adjacent to title._
 ## Setup/Installation Requirements
 * _Clone Repo_
 * _NPM Install_
--- store.js
+++ store.js
@@ -4,47 +4,50 @@ const readFileAsync = util.promisify(fs.readFile)
 const writeFileAsync = util.promisify(fs.writeFile)
 
 class Store {
-    constructor(){
+    constructor() {
         this.Id = 0
     }
-read(){
-    return readFileAsync('./db.json', 'utf8')
-}
-    write(note){
+    read() {
+        return readFileAsync('./db.json', 'utf8')
+    }
+    write(note) {
         return writeFileAsync('./db.json', JSON.stringify(note))
     }
-    getnote(){
+    getnote() {
         return this.read().then(notes => {
             let parseNotes;
-            try{
+            try {
                 parseNotes = [].concat(JSON.parse(notes))
-            }catch(err){
+            } catch (err) {
                 //parseNotes[];
             }
             return parseNotes
         })
     }
-    addnote(note){const { title, text } = note; //es6 deconstructed
-    if (!title || !text) {
-      throw new Error("Note 'title' and 'text' cannot be blank");
-    }
-    // Increment `this.lastId` and assign it to `newNote.id`
-    const newNote = { title, text, id: ++this.lastId };
-    // Get all notes, add the new note, write all the updated notes, return the newNote
-    return this.getnote()
-      .then(notes => [...notes, newNote])//spread operator
-      .then(updatedNotes => this.write(updatedNotes))
-      .then(() => newNote);
-
-}
-    removenote(id){// Get all notes, remove the note with the given id, write the filtered notes
+    addnote(note) {
+        const { title, text } = note; //es6 deconstructed
+        if (!title || !text) {
+            throw new Error("Note 'title' and 'text' cannot be blank");
+        }
+        // Increment `this.lastId` and assign it to `newNote.id`
+        const newNote = { title, text, id: ++this.lastId };
+        // Get all notes, add the new note, write all the updated notes, return the newNote
         return this.getnote()
-          .then(notes => notes.filter(note => note.id !== parseInt(id)))
-          .then(filteredNotes => this.write(filteredNotes));
-    
-    
-    
-    
-    
+            .then(notes => [...notes, newNote])//spread operator
+            .then(updatedNotes => this.write(updatedNotes))
+            .then(() => newNote);
+
     }
-} module.exports = new Store()
+    removenote(id) {// Get all notes, remove the note with the given id, write the filtered notes
+        return this.getnote()
+            .then(notes => notes.filter(note => note.id !== parseInt(id)))
+            .then(filteredNotes => this.write(filteredNotes));
+
+
+
+
+
+    }
+}
+
+module.exports = new Store()
